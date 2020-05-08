11c7d40bff7a015c087ded433d84708b186ab6e4
George <quarelli.george@gmail.com>
2020-05-08 08:32:36 +0200

    js

---
+++ main.js
@@ -0,0 +1,17 @@
+for (var i = 0; i < 25; i++) {
+    $('#griglia').append('<div class="quadrato"></div>');
+}
+
+function assegna_numero(){
+    $( ".quadrato" ).each(function(){
+        var numero = genera_random(1,10)
+        console.log(numero);
+        $(this).append(numero);
+    });
+
+}
+assegna_numero();
+
+function genera_random (min, max){
+return Math.floor(Math.random() *(max-min+1)) + min;
+}
